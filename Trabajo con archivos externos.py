dc507eefdf52b94507400ac9e663ebf59c3c808a
Luis Rosado <iscluisars@gmail.com>
2025-02-26 12:39:49 -0600

    No leer la primera linea pero si el resto. Metodo seek y readLine

--- Trabajo con archivos externos.py
+++ Trabajo con archivos externos.py
@@ -2,6 +2,8 @@ from io import open
 
 archivo_externo = open("primerArchivo.txt","r")
 
-archivo_externo.seek(len(archivo_externo.read())/2)
+#archivo_externo.seek(len(archivo_externo.read())/2)
+
+archivo_externo.seek(len(archivo_externo.readline()))
 
 print(archivo_externo.read())
