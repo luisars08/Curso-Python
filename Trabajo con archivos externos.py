db610b6c5b2fa72b1db223563e8d9364fc5b3144
Luis Rosado <iscluisars@gmail.com>
2025-02-26 10:25:02 -0600

    Leer por line o lineas con el metodo readLine() o readLines()

--- Trabajo con archivos externos.py
+++ Trabajo con archivos externos.py
@@ -1,7 +1,11 @@
 from io import open
 
-archivo_externo = open("primerArchivo.txt","a")
+archivo_externo = open("primerArchivo.txt","r")
 
-archivo_externo.write("\nGuardamos información de forma permanente")
+#archivo_externo.write("\nGuardamos información de forma permanente")
+
+informacion_lineas = archivo_externo.readlines()
 
 archivo_externo.close()
+
+print(informacion_lineas[1])
