180959d2510eb24eac579ff9b6aecdfe370f5598
Luis Rosado <iscluisars@gmail.com>
2025-02-26 12:36:44 -0600

    Leer la mitad del documento con metodo seek y len

--- Trabajo con archivos externos.py
+++ Trabajo con archivos externos.py
@@ -2,8 +2,6 @@ from io import open
 
 archivo_externo = open("primerArchivo.txt","r")
 
-print(archivo_externo.read())
-
-archivo_externo.seek(0)
+archivo_externo.seek(len(archivo_externo.read())/2)
 
 print(archivo_externo.read())
