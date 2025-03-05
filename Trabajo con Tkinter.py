0654253aab7865526254c9dba8e1b662285792eb
Luis Rosado <iscluisars@gmail.com>
2025-03-05 10:11:06 -0600

    Curso Python Tkinter cambiar titulo, tamaño, icono, background

---
+++ Trabajo con Tkinter.py
@@ -0,0 +1,16 @@
+from tkinter import * 
+
+raiz = Tk()
+
+raiz.title("Primera ventana Py")
+
+raiz.resizable(1,1)
+
+raiz.iconbitmap("favicon.ico")
+
+raiz.geometry("700x400")
+
+raiz.configure(bg="#008491")
+
+raiz.mainloop()
+
