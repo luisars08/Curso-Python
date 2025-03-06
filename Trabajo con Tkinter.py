3fc0eab1fce33b220cab0689ee23903756d528c4
Luis Rosado <iscluisars@gmail.com>
2025-03-06 14:47:54 -0600

    Base de Tkinter <crear raiz y frame>

--- Trabajo con Tkinter.py
+++ Trabajo con Tkinter.py
@@ -8,9 +8,17 @@ raiz.resizable(1,1)
 
 raiz.iconbitmap("favicon.ico")
 
-raiz.geometry("700x400")
+#raiz.geometry("700x400") Ya no es necesario porque ahora toma el tamaño del frame
 
 raiz.configure(bg="#008491")
+
+miFrame = Frame() #Programando el Frame
+
+miFrame.pack(expand="True" ,fill="y")  #Empaquetando
+
+miFrame.config(bg="red") #Darle color al Frame
+
+miFrame.config(width="650", height="350")
 
 raiz.mainloop()
 
