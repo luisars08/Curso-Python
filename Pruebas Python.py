3dbf0c9af6a5792dc5796e15c89e661ee25c3706
Luis Rosado <iscluisars@gmail.com>
2025-03-09 14:54:49 -0600

    <CW> Quitar repetido de una cadena

--- Pruebas Python.py
+++ Pruebas Python.py
@@ -1,11 +1,21 @@
-def filtro_lista(lista):
-   
-   return [x for x in lista if isinstance(x, int)]
 
-   
+def Unique_in_order(sequence):
+    if not sequence:
+        return []
+    
+    # Inicializamos la lista con el primer elemento
+    unique_list = [sequence[0]]
+    
+    # Recorremos desde el segundo elemento
+    for i in range(1, len(sequence)):
+        # Si el elemento actual es diferente al anterior, lo agregamos
+        if sequence[i] != sequence[i-1]:
+            unique_list.append(sequence[i])
+    
+    return unique_list
 
-lista = [1,2,'a','b']
-
-lista2 = [1,2,'aasf','1','123',123]
-
-print(filtro_lista(lista2))
+print(Unique_in_order('AAAABBBCCDAABBB'))
+print(Unique_in_order([1, 2, 2, 3, 3]))
+print(Unique_in_order('ABBCcAD'))
+print(Unique_in_order([]))
+print(Unique_in_order('A'))     
