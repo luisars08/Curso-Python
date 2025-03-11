c9d9dde70020e72dc65dddf9f38189a8f1d26ca8
Luis Rosado <iscluisars@gmail.com>
2025-03-11 16:02:47 -0600

    Se deja el código como el del profe

--- Trabajo con Entry.py
+++ Trabajo con Entry.py
@@ -2,26 +2,32 @@ from tkinter import *
 
 root = Tk()
 
-myFrame = Frame(root, width=750, height=550)
+myFrame = Frame(root, width=1000, height=550)
 myFrame.pack()
 
-nombreLabel = Label(myFrame,text="Nombre: ")
-nombreLabel.grid(row=0, column=0, sticky=W)
+cuadroTextoNombre=Entry(myFrame)
+cuadroTextoNombre.grid(row=0,column=1)
+cuadroTextoNombre.config(fg="red")
 
-cuadroTexto = Entry(myFrame)
-cuadroTexto.grid(row=0, column=1)
-cuadroTexto.config(fg="red")
+cuadroTextoApellido=Entry(myFrame)
+cuadroTextoApellido.grid(row=1,column=1)
 
-apellidoLb = Label(myFrame,text="Apellido: ").grid(row=1,column=0, sticky=W)
-apellidoEt = Entry(myFrame)
-apellidoEt.grid(row=1,column=1)
-apellidoEt.config(fg="red")
+cuadroTextoMail=Entry(myFrame)
+cuadroTextoMail.grid(row=2,column=1)
 
-mailLb = Label(myFrame,text="Mail: ").grid(row=2,column=0, sticky=W)
-mailEt = Entry(myFrame).grid(row=2,column=1)
+cuadroTextoDireccion=Entry(myFrame)
+cuadroTextoDireccion.grid(row=3,column=1)
 
-direccionLb = Label(myFrame,text="Dirección del domicilio: ").grid(row=3,column=0, sticky=W)
-direccionEt = Entry(myFrame).grid(row=3,column=1)
+nombreLabelNombre=Label(myFrame, text="Nombre: ")
+nombreLabelNombre.grid(row=0, column=0, sticky="w")
 
+nombreLabelApellido=Label(myFrame, text="Apellido: ")
+nombreLabelApellido.grid(row=1, column=0, sticky="w")
+
+nombreLabelMail=Label(myFrame, text="Mail: ")
+nombreLabelMail.grid(row=2, column=0, sticky="w")
+
+nombreLabelDireccion=Label(myFrame,text="Direccion de casa: ")
+nombreLabelDireccion.grid(row=3, column=0, sticky="w")
 
 root.mainloop()
