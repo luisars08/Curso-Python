eea97df2ae00565ea1b1edf75501e26782729a22
Luis Rosado <iscluisars@gmail.com>
2025-04-04 15:57:08 -0600

    Ahora el simbolo ^ hace de negador de la busqueda en el rango

--- Trabajo con regex.py
+++ Trabajo con regex.py
@@ -4,6 +4,6 @@ lista_terminos = ["Ma1","Se1","Ma2","Va1","Ba1","Se2","Ma3","Ma4"]
 
 for termino in lista_terminos:
 
-    if re.findall("Ma[1-3]",termino):
+    if re.findall("Ma[^1-3]",termino):
         print(termino)
 
