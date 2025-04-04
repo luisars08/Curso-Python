f57a4f39177bfdf9c29f927a33c3ca9050eadd4c
Luis Rosado <iscluisars@gmail.com>
2025-04-04 15:56:24 -0600

    Buscar con rango

--- Trabajo con regex.py
+++ Trabajo con regex.py
@@ -1,9 +1,9 @@
 import re
 
-lista_terminos = ["camión", "camion", "niños","niñas","ejemplo"]
+lista_terminos = ["Ma1","Se1","Ma2","Va1","Ba1","Se2","Ma3","Ma4"]
 
 for termino in lista_terminos:
 
-    if re.findall("[l-p]$",termino):
+    if re.findall("Ma[1-3]",termino):
         print(termino)
 
