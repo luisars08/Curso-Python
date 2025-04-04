47192e02abcea5665035defa1427565156d60d62
Luis Rosado <iscluisars@gmail.com>
2025-04-04 16:23:36 -0600

    Trabajando con rangos de letras y números

--- Trabajo con regex.py
+++ Trabajo con regex.py
@@ -1,9 +1,9 @@
 import re
 
-lista_terminos = ["Ma1","Se1","Ma2","Va1","Ba1","Se2","Ma3","Ma4"]
+lista_terminos = ["Ma1","Se1","Ma2","Va1","Ba1","Se2","Ma3","Ma4","Se3","SeA","SeB","Va2","SeC"]
 
 for termino in lista_terminos:
 
-    if re.findall("Ma[^1-3]",termino):
+    if re.findall("Se[0-2A-B]",termino):
         print(termino)
 
