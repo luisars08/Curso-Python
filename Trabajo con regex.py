7aa52df89c64a27aa690782c252c49455dbf53aa
Luis Rosado <iscluisars@gmail.com>
2025-04-04 16:26:46 -0600

    Busqueda con . : tipo archivos csv

--- Trabajo con regex.py
+++ Trabajo con regex.py
@@ -1,9 +1,9 @@
 import re
 
-lista_terminos = ["Ma1","Se1","Ma2","Va1","Ba1","Se2","Ma3","Ma4","Se3","SeA","SeB","Va2","SeC"]
+lista_terminos = ["Ma:1","Se1","Ma2","Va1","Ba1","Se2","Ma.3","Ma4","Se:3","SeA","SeB","Va2","SeC"]
 
 for termino in lista_terminos:
 
-    if re.findall("Se[0-2A-B]",termino):
+    if re.findall("Ma[.:]",termino):
         print(termino)
 
