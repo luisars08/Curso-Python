c130dc63bcac8c8a286cc92a22fba256936c6b77
Luis Rosado <iscluisars@gmail.com>
2025-04-08 13:51:09 -0600

    Primer ejemplo funcion LAMBDA

---
+++ Trabajo con lambda.py
@@ -0,0 +1,11 @@
+def calcula_area_triangulo(b,h):
+ return (b*h)/2
+
+print("El área es de: ", calcula_area_triangulo(10,5))
+
+
+area_triangulo = lambda base,altura: (base*altura)/2
+
+print(area_triangulo(10,5))
+
+print(area_triangulo(3,6))
