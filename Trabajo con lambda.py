b42fded315eb8ceb3960e888c1e538839d342016
Luis Rosado <iscluisars@gmail.com>
2025-04-10 11:04:47 -0600

    Ejemplo normal sin lambda

--- Trabajo con lambda.py
+++ Trabajo con lambda.py
@@ -1,5 +1,9 @@
-formato = lambda comision: "¡{}!$".format(comision)
 
-comision_Antonio = 6000
+mi_lista = [(11,5),(15,7),(2,4),(15,19),(8,4)]
 
-print(formato(comision_Antonio))
+def ordena_lista(t):
+    return t[0]+t[1]
+
+mi_lista.sort(key=ordena_lista)
+
+print(mi_lista)
