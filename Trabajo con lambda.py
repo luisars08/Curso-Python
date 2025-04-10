eca746e21de5b546f1177faa6a1789ddcd6ac468
Luis Rosado <iscluisars@gmail.com>
2025-04-10 11:06:17 -0600

    Ejemplo con Lambda

--- Trabajo con lambda.py
+++ Trabajo con lambda.py
@@ -1,9 +1,9 @@
 
 mi_lista = [(11,5),(15,7),(2,4),(15,19),(8,4)]
 
-def ordena_lista(t):
-    return t[0]+t[1]
+"""def ordena_lista(t):
+    return t[0]+t[1]"""
 
-mi_lista.sort(key=ordena_lista)
+mi_lista.sort(key=lambda t:t[0]+t[1])
 
 print(mi_lista)
