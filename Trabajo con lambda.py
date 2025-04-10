6cdd6635cb6014e98608e8ee239f7b17546cabd9
Luis Rosado <iscluisars@gmail.com>
2025-04-10 11:25:57 -0600

    Ejemplo 5 con LAMBDA

--- Trabajo con lambda.py
+++ Trabajo con lambda.py
@@ -1,9 +1,9 @@
 
 musicos = ["Paul McCartney","Bruce Springsteen", "Tina Turner", "Justin Bieber", "Elton Jhon"]
 
-def ordena_musicos(m):
-    return m.split()[1]
+#def ordena_musicos(m):
+    #return m.split()[1]
 
-musicos.sort(key=ordena_musicos)
+musicos.sort(key=lambda m:m.split()[1])
 
 print(musicos)
