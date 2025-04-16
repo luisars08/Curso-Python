b31d681b703e9c559ca8111dcda734769edfb2b2
Luis Rosado <iscluisars@gmail.com>
2025-04-16 11:06:30 -0600

    Ejercicio 1 Funciones Lambda

--- Ejercicio lambda 1.py
+++ Ejercicio lambda 1.py
@@ -1,9 +1,4 @@
 
-frase1="Los lunes son los mejores días para programar"  #45  4to
-frase2="Python es moderno"                              #17  1ro
-frase3="Veremos Inteligencia Artificial más adelante"   #44  3ro
-frase4="Lambda simplifica el código"                    #27  2do
-
 lista_frases = ["Los lunes son los mejores días para programar","Python es moderno","Veremos Inteligencia Artificial más adelante","Lambda simplifica el código" ]
 
 '''def cuenta_letras(f):
