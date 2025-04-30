be431b15b7b451501b9884670b1a09b3f8ae035b
Luis Rosado <iscluisars@gmail.com>
2025-04-30 13:20:48 -0600

    Insertar varios registros a la vez <executemany()>

--- Trabajo BBDD/Practica Datos.py
+++ Trabajo BBDD/Practica Datos.py
@@ -6,7 +6,16 @@ miCursor = miConexion.cursor()
 
 #miCursor.execute("CREATE TABLE PRODUCTOS (NARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
 
-miCursor.execute("INSERT INTO PRODUCTOS VALUES('Camiseta',25,'Moda')")
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES('Camiseta',25,'Moda')")
+
+muchosProductos = [
+
+    ("Patin",100,"Deportes"),
+    ("Cenicero",20,"Cerámicas"),
+    ("Pantalón",80,"Moda")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES(?,?,?)",muchosProductos)
 
 miConexion.commit()
 
